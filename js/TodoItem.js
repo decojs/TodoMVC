15d6a6a1b48f580f59b4f27fcc7bfc02ec5d4615
Marius Gundersen <me@mariusgundersen.net>
2014-02-05 19:47:55 +0100

    Added editing

--- js/TodoItem.js
+++ js/TodoItem.js
@@ -5,11 +5,22 @@ define([
 ){
   
   return function TodoItem(todo){
+    var self = this;
 
     this.completed = ko.observable(false);
     this.editing = ko.observable(false);
 
     this.content = ko.observable(todo || "");
+
+    this.content.subscribe(function(value){
+      if(value){
+        self.editing(false);
+      }
+    })
+
+    this.edit = function(){
+      self.editing(true);
+    };
 
   };
 
